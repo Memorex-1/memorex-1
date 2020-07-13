f69f2c39dabf86c88e0a6bd4fe448aeb65d43a14
Maurizio <maurizio_1999@outlook.es>
2020-07-12 22:35:15 -0500

    beta 2.0

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask,render_template,redirect,request,url_for
+from flask import Flask,render_template,redirect,request,url_for,session,flash
 from flask_sqlalchemy import SQLAlchemy
+import bcrypt
 app = Flask(__name__)
+app.secret_key = "appLogin"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/task1.db'
-
+semilla = bcrypt.gensalt()
 db = SQLAlchemy(app)
+db1 = SQLAlchemy(app)
 class personajes(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     nombre = db.Column(db.String(20))
@@ -11,20 +14,92 @@ class personajes(db.Model):
     hecho = db.Column(db.String(100))
     pro = db.Column(db.Integer)
     fuente = db.Column(db.String(50))
+class usuarios(db1.Model):
+    __tablename__ = 'login'
+    id = db1.Column(db.Integer,primary_key=True)
+    nombre = db1.Column(db.String(20))
+    apellido = db1.Column(db.String(20))
+    correo = db1.Column(db.String(50))
+    contraseña = db1.Column(db.String(50))
 
 @app.route('/')
 def index():
     return render_template('index.html')
+#funcion que te lleva al login.html
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    if(request.method == "GET"):
+        if 'name' in session:
+            return redirect(url_for('editor'))
+        else:
+            return render_template('login.html')
+    else:
+        return render_template('login.html')
+
+#funcion para ingresar a la pagina 
+@app.route('/ingresar',methods = ['POST','GET'])
+def ingresar():
+    if(request.method == "GET"):
+        if 'name' in session:
+            return redirect(url_for('editor'))
+        else:
+            return render_template('login.html')
+    else:
+        #obtiene datos 
+        correo = request.form['username']
+        password = request.form['password']
+        password_encode = password.encode("utf-8")
+        usuario = usuarios.query.filter_by(correo = correo).first()
+        if(usuario !=None):
+            #obtiene el password encriptado encode
+            password_encriptado_encode = usuario.contraseña
+            #verifica el password
+            if(bcrypt.checkpw(password_encode,password_encriptado_encode)):
+                #registra la sesion
+                session['name'] = usuario.nombre
+                return redirect(url_for('editor'))
+            else:
+                return render_template('login.html')
+        else:
+            return render_template('login.html')
 @app.route('/RegistrarUsuario')
 def registrar():
-    return render_template('RegistrarUsuario.html')
-@app.route('/editor-personaje')
+    if(request.method == "GET"):
+        if 'name' in session:
+            return redirect(url_for('editor'))
+        else:
+            return render_template('RegistrarUsuario.html')
+    else:
+        return render_template('RegistrarUsuario.html')
+@app.route('/editor-personaje', methods = ['POST','GET'])
 def editor():
     tasks = personajes.query.all()
     return render_template('editor-personaje.html', tasks = tasks)
+
+# funcion que registra los datos en la bd 
+@app.route('/registro-usuario', methods = ['POST','GET'])
+def registro():
+    if(request.method == "GET"):
+        if 'name' in session:
+            return redirect(url_for('editor'))
+        else:
+            return render_template('RegistrarUsuario.html')
+    else:
+        nombre = request.form['name']
+        apellido = request.form['firtsname']
+        correo = request.form['email']
+        contraseña = request.form['password']
+        password_encode = contraseña.encode("utf-8")
+        password_ecriptado = bcrypt.hashpw(password_encode,semilla)
+
+        task = usuarios(nombre = nombre, apellido = apellido, correo = correo, contraseña = password_ecriptado )
+        db.session.add(task)
+        db.session.commit()
+        #registra la sesion 
+        session['name'] = nombre
+        #session['firstname'] = apellido
+        #session['email'] = correo
+        return render_template('login.html')
 
 @app.route('/crear', methods = ['POST'])
 def pagina():
@@ -38,6 +113,10 @@ def referenica():
     task.informacion = request.form['descripcion']+" "
     db.session.commit()
     return redirect(url_for('editor'))
+@app.route('/salir')
+def salir():
+    session.clear()
+    return redirect(url_for('index'))
 
 if __name__=='__main__':
     app.run(debug=True, port=5000)
